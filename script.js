f7c56992d9cab2d94d9cdec63b0803f0c89cacff
xuziang111 <584788096@qq.com>
2017-12-04 20:59:35 +0800

    slidupanime

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 works1.onclick=function(){
   worksBar.className='state1';
 };
@@ -7,6 +8,8 @@ works2.onclick=function(){
 works3.onclick=function(){
   worksBar.className='state3';
 };
+var obj = {x:1,minIndex:0,Y:0} ;
+obj.Y=window.scrollY;
 window.onscroll=function(){
   if(window.scrollY>0){      
     topNavBarIn.classList.add('stick');       
@@ -15,23 +18,28 @@ window.onscroll=function(){
   }
 
   let specialTags = document.querySelectorAll('[data-y]');
-  let minIndex = 0;
-  for(let i=1;i<specialTags.length;i++){
-    if(Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)){
-      minIndex=i;
-      console.log(minIndex);
-      console.log(Math.abs(specialTags[i].offsetTop-window.scrollY));
-      console.log(Math.abs(specialTags[minIndex].offsetTop-window.scrollY));
+  if(obj.Y<window.scrollY&&specialTags[obj.minIndex+1].offsetTop - window.scrollY-window.innerHeight <= 0){
+      obj.minIndex=obj.x;
+      specialTags[obj.x].classList.add('active');
+      if(obj.x<2){
+      obj.x=obj.x+1;
     }
-  }
-  specialTags[minIndex].classList.remove('offset');
-  let li = document.querySelector('a[href="#' + specialTags[minIndex].id + '"]').parentNode;
+    }else  if (obj.Y>window.scrollY&&specialTags[obj.minIndex].offsetTop - window.scrollY-window.innerHeight > 0&&obj.x>0){      
+      specialTags[obj.x].classList.remove('active');
+      obj.x=obj.x-1;
+      specialTags[obj.x].classList.add('active');
+      obj.minIndex=obj.x;
+      
+    }
+  
+
+  let li = document.querySelector('a[href="#' + specialTags[obj.minIndex].id + '"]').parentNode;
   let brothers = li.parentNode.children;
-  for(let i=0;i<brothers.length;i++){
-    brothers[i].classList.remove('highlight');
+  for(let k=0;k<brothers.length;k++){
+    brothers[k].classList.remove('highlight');
   }
   li.classList.add('highlight');
-
+  obj.Y=window.scrollY;
 }  
 
 let liTags = document.getElementsByTagName('LI');  
@@ -89,7 +97,7 @@ for(let i=0;i<aTags.length;i++){
   }
 } 
 
-
+window.scrollTo(0,1);
 loading.classList.remove('loading');
 
 
