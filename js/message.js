82a51b15f589795ccd657619ce56882965f55d8f
xuziang111 <584788096@qq.com>
2018-02-21 22:18:30 +0800

    mvc的m

--- js/message.js
+++ js/message.js
@@ -1,48 +1,70 @@
+!function(){
+	var view = document.querySelector("#leaveMessage")
+	var model = {
+		init:function(){
+      var APP_ID = 'orldLePcUlN3v9H2DehyMz5V-gzGzoHsz';
+      var APP_KEY = 'DyCcYr2vF8Du4kR5mhmvFjsG';         
+      AV.init({
+        appId: APP_ID,
+        appKey: APP_KEY
+        });            
+      },
+		fetch:function(){
+			var query = new AV.Query('Message');
+			return query.find() //promise对象
+		},
+		save:function(name,content){
+			var Message = AV.Object.extend('Message'); 
+					var message = new Message();
+					return message.save({ //promise对象
+						'name':name,
+						'content': content
+					})
+		}
+	}
 
-var APP_ID = 'orldLePcUlN3v9H2DehyMz5V-gzGzoHsz';
-var APP_KEY = 'DyCcYr2vF8Du4kR5mhmvFjsG';
+  var controller ={
+		view:null,
+		messageList:null,
+    init:function(view){
+			this.view = view;
+			this.model = model;
+			this.model.init();
+			this.messageList = view.querySelector('#messageList')
+			this.form = view.querySelector('form')
+			this.loadMessages();
+			this.bindEvents();
+    },
+      loadMessages:function(){
+        this.model.fetch().then( (message) => {
+						console.log(message)
+						let array = message.map((item) => item.attributes )
+        	  array.forEach((item)=>{
+							let li = document.createElement('li');
+        	    li.innerText = `${item.name}:${item.content}`;
+        	    this.messageList.appendChild(li)
+          	})
+        	},function(error){
+          alert('error')
+        });            
+			},
+			bindEvents:function(){
+				let myForm = this.form
+				myForm.addEventListener('submit',function(e){
+					e.preventDefault();
+					let name = myForm.querySelector('input[name=name]').value;
+					let content = myForm.querySelector('input[name=content]').value;
+					console.log(content)
+					this.model.save(name,content).then(function(object) {
+						let li = document.createElement('li');
+						li.innerText = `${object.attributes.name}:${object.attributes.content}`;
+						this.messageList.appendChild(li)
+					})
+			})
+		},
+		x:function(){
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-
-/*
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
-})
-*/
-
-var query = new AV.Query('Message');
-query.find().then(function (message) {
-    message.forEach(function(item){
-        console.log(item.attributes.content)
-        let li = document.createElement('li');
-        li.innerText = item.attributes.content;
-        messageList.appendChild(li)
-    })
-  },function(error){
-      alert('error')
-  });
-
-
-let myFrom = document.querySelector('#postMessageForm')
-console.log(myFrom)
-myFrom.addEventListener('submit',function(e){
-    console.log('no error');
-    e.preventDefault();
-    let content = myFrom.querySelector('input[name=content]').value;
-    console.log(content)
-    var Message = AV.Object.extend('Message'); 
-    var message = new Message();
-    message.save({
-     'content': content
-    }).then(function(object) {
-        console.log('no error');
-        console.log(object);
-    })
-})
+		}	
+  }
+controller.init(view);
+}.call()
